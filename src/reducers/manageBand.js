bac120cc143c05f65aea122703d3f7433dae045d
claudiahalip <claudia4ro@yahoo.com>
2020-09-19 17:07:21 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,16 @@
+export const addBand = (band) => {
+  return { type: "ADD_BAND", payload: band }
+}
+
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+  switch (action.type){
+    case "ADD_BAND":
+      return {bands: [...state.bands, action.payload]}
+  default:
+    return state;
+  }
 };
+
+
